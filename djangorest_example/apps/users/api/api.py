8c33e729371772f44bc3666178a098488df25923
Miguel Bareno <miguel.bareno@hotmail.com>
2022-07-21 16:51:59 -0500

    Added status code for GET, POST, PUT, DELETE

--- djangorest_example/apps/users/api/api.py
+++ djangorest_example/apps/users/api/api.py
@@ -1,5 +1,4 @@
-from operator import is_not
-import re
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
@@ -11,7 +10,7 @@ class UserAPIView(APIView):
     def get(self, request):
         users = User.objects.all()
         user_serializer = UserSerializer(users, many = True)
-        return Response(user_serializer.data)
+        return Response(user_serializer.data, status = status.HTTP_200_OK)
 
 @api_view(['GET','POST'])
 def user_decorator_api_view(request):
@@ -19,14 +18,14 @@ def user_decorator_api_view(request):
     if request.method == 'GET':
         users = User.objects.all()
         users_serializer = UserSerializer(users, many = True)
-        return Response(users_serializer.data)
+        return Response(users_serializer.data, status = status.HTTP_200_OK)
 
     elif request.method == 'POST':
         user_serializer = UserSerializer(data = request.data)
 
         if user_serializer.is_valid():
             user_serializer.save()
-            return Response(user_serializer.data)
+            return Response(user_serializer.data, status = status.HTTP_201_CREATED)
 
         return Response(user_serializer.errors)
 
@@ -37,7 +36,7 @@ def user_detail_view(request, pk = None):
         if pk is not None:
             user = User.objects.filter(id = pk).first()
             user_serializer = UserSerializer(user)
-            return Response(user_serializer.data)
+            return Response(user_serializer.data, status = status.HTTP_200_OK)
         else:
             print("pk vacio")
 
@@ -47,10 +46,10 @@ def user_detail_view(request, pk = None):
             user_serializer = UserSerializer(user, data = request.data)
             if UserSerializer.is_valid():
                 user_serializer.save()
-                return Response(user_serializer.data)
+                return Response(user_serializer.data, status = status.HTTP_202_ACCEPTED)
             return Response(user_serializer.errors)
 
     elif request.method == 'DELETE':
         user = User.objects.filter(id = pk).first()
         user.delete()
-        return Response('usuario id = {} fue elemininado '.format(pk))
+        return Response('usuario id = {} fue elemininado '.format(pk), status = status.HTTP_202_ACCEPTED)
